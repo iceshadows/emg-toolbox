c5d8da57c952d794105419d48192ee63be304ecb
张淋 <zhanglin@wearlab.tech>
2024-10-20 21:30:03 +0800

    更新setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
 
+# 读取 README.md 文件的内容
+with open(os.path.join(os.path.dirname(__file__), 'README.md'), 'r', encoding='utf-8') as f:
+    long_description = f.read()
 setup(
     name='emg_toolbox',
-    version='1.0.0',
+    version='1.0.1',
     author='Linus Zhang',
     author_email='products@wearlab.tech',
-    description='A description of your library',
-    long_description='A longer description of your library',
+    description='EMG Toolbox is a Python toolkit for processing and analysing surface electromyography (sEMG) data. It includes a variety of feature extraction methods, signal filtering, and plotting functions, helping users efficiently preprocess and analyse EMG signals.',
+    long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/your_username/your_repository',
     packages=['emg_toolbox'],
@@ -29,4 +33,5 @@ setup(
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
     ],
+    python_requires='>=3.6',
 )
