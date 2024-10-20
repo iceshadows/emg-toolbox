9b313ec06a41f7ca95be1e05394ff9fe7ab7c8f1
张淋 <zhanglin@wearlab.tech>
2024-10-20 20:44:00 +0800

    First Version

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup
+
+setup(
+    name='emg-toolbox',
+    version='0.1.0',
+    author='Linus Zhang',
+    author_email='products@wearlab.tech',
+    description='A description of your library',
+    long_description='A longer description of your library',
+    long_description_content_type='text/markdown',
+    url='https://github.com/your_username/your_repository',
+    packages=['emg-toolbox'],
+    install_requires=[
+        'pandas',
+        'seaborn',
+        'matplotlib',
+        'numpy',
+        'scipy',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
